affaa1ed4877a2f003c37d06bdb729d647c444b2
yuvrajchowdary <40199249+yuvrajchowdary@users.noreply.github.com>
2018-07-18 01:00:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// GENERAL SETTING
+window.sr = ScrollReveal({
+    reset: true
+});
+
+// Custom Settings
+sr.reveal('.foo-1', { 
+    origin: 'right', 
+    duration: 2000 
+  });
+sr.reveal('.foo-2', {
+    origin: 'right',
+    duration: 2000
+});
+sr.reveal('.foo-3', {
+    origin: 'right',
+    duration: 2000
+});
+sr.reveal('.foo-4', {
+    origin: 'right',
+    duration: 2000
+});
+sr.reveal('.foo-5', {
+    origin: 'right',
+    duration: 2000
+});
